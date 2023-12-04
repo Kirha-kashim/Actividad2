a14d7b45c08002bb68da10af04ad0c6658268438
Karen_Plata <platak140@gmail.com>
2023-12-03 20:23:32 -0400

    subasta

---
+++ Persona.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Persona here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Persona
+{
+     private String nombre;
+    {
+        this.nombre = nombre;
+    }
+    
+    public void setPersona(String nombre){
+        this.nombre = nombre;
+    }
+    public String getPersona(){
+        return nombre;
+    }
+}
